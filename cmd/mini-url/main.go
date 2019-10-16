4745c4be559d810f5aad88cc8c21fb38895114bd
Lorna Song <lorna.song@gmail.com>
2019-10-15 23:18:36 -0400

    add env config

--- cmd/mini-url/main.go
+++ cmd/mini-url/main.go
@@ -4,15 +4,20 @@ import (
 	"log"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo"
 	"github.com/lornasong/mini-url/src/handlers"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	c, err := LoadConfig()
+	if err != nil {
+		log.Fatalln("Error loading configs", err)
+	}
 
+	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		log.Fatalln("$PORT must be set")
 	}
 
 	e := echo.New()
@@ -20,4 +25,20 @@ func main() {
 	e.POST("/mini", handlers.Generate())
 	e.GET("/mini/:id", handlers.GoTo())
 	e.Start(":" + port)
+}
+
+// Config TODO:
+type Config struct {
+	BaseURL string `envconfig:"BASE_URL" required:"true`
+	APIPath string `envconfig:"API_PATH" required:"true`
+}
+
+// LoadConfig loads the configuration object based on the environment and other defaults.
+func LoadConfig() (*Config, error) {
+	var c Config
+	err := envconfig.Process("", &c)
+	if err != nil {
+		return nil, err
+	}
+	return &c, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/lornasong/mini-url
 go 1.12
 
 require (
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 )
