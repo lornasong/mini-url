851f7e9be0f7e5890d3babacea2e0dc0fe29c609
Lorna Song <lorna.song@gmail.com>
2019-10-15 21:15:12 -0400

    try adding port variable for heroku

--- cmd/mini-url/main.go
+++ cmd/mini-url/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/labstack/echo"
 	"github.com/lornasong/mini-url/src/handlers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	e := echo.New()
 	e.File("/miniurl", "public/index.html")
 	e.POST("/mini", handlers.Generate())
 	e.GET("/mini/:id", handlers.GoTo())
-	e.Start(":8000")
+	e.Start(":" + port)
 }
